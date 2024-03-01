3c0093c14c65f2ad70c2709ce9f87e074e8775f1
nikhil-kumar-tomar <nikhiltomar931@gmail.com>
2024-03-01 17:20:20 +0530

    WebRTC handshake done

--- public/js/rtcpeerscript.js
+++ public/js/rtcpeerscript.js
@@ -1,11 +1,48 @@
-const peer = new SimplePeer({ initiator: true });
 
-peer.on('signal', data => {
-    console.log('Sender Signal:', data);
+const peerSender = new SimplePeer({
+  initiator: true, 
+  config: {
+    iceServers: [
+      { urls: "stun:stun1.l.google.com:19302"},
+      { urls: "stun:stun2.l.google.com:19302"},
+      { urls: "stun:stun3.l.google.com:19302"},
+      { urls: "stun:stun4.l.google.com:19302"},
+    ] },
+    trickle:false
 });
 
-
-document.getElementById('fileInput').addEventListener('change', () => {
-    let data = 'Hello from sender!';
-    peer.send(data);
+peerSender.on('signal', data => {
+    // Send the offer to the receiver
+    // (You'll need to implement a method to send this offer to the receiver)
+    // peerSender.
+    console.log(data)
+    document.getElementById("offers").value += JSON.stringify(data)
+    // sendOfferToReceiver(data);
 });
+
+peerSender.on('connect', () => {
+    console.log('Sender: Connection established');
+    // You can start sending data here
+    peerSender.send('Hello from sender!');
+});
+
+// peer.on('signal', data => {
+//     console.log('Sender Signal:', data);
+//     document.getElementById("offers").value += JSON.stringify(data)
+// });
+
+// // The peer.signal is for sending data to other peer
+// peer.signal(data);
+
+
+// document.getElementById('fileInput').addEventListener('change', () => {
+//     let data = 'Hello from sender!';
+//     peer.send(data);
+// });
+
+let some = () => {
+    console.log(JSON.parse(document.getElementById("offers").value))
+    peerSender.signal(document.getElementById("offers").value)
+}
+
+document.getElementById('accept-button').addEventListener('click', some)
--- public/js/rtcreceiverscript.js
+++ public/js/rtcreceiverscript.js
@@ -1,11 +1,40 @@
-const peer = new SimplePeer();
+
+
+const peer = new SimplePeer({
+    config: {
+        iceServers: [
+        { urls: "stun:stun1.l.google.com:19302"},
+        { urls: "stun:stun2.l.google.com:19302"},
+        { urls: "stun:stun3.l.google.com:19302"},
+        { urls: "stun:stun4.l.google.com:19302"},
+      ]
+    }
+}
+);
+
+let some = () => {
+    peer.signal(document.getElementById("answers").value)
+}
 
 peer.on('signal', data => {
-    console.log('Receiver Signal:', data);
-});
+    console.log(data);
+    document.getElementById("answers").value = JSON.stringify(data)
+})
+
+peer.on('data', data => {
+    // got a data channel message
+    console.log('got a message from peer1: ' + data)
+  })
+  
+document.getElementById('answer-button').addEventListener('click', some)
+// peer.on('signal', data => {
+//     console.log('Receiver Signal:', data);
+// });
+
 
 // Handling received messages
-peer.on('data', data => {
-    document.getElementById('download-button').innerText = data.toString();
-    console.log('Received Message:', data.toString());
-});
+// peer.on('signal', data => {
+//     console.log(data);
+    // document.getElementById('download-button').innerText = data.toString();
+    // console.log('Received Message:', data.toString());
+// });
